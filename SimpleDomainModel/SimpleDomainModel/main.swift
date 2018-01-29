2c1e7ba58099b9aa85ceaa622394905ab5fe4577
leontaolong <longt8@uw.edu>
2018-01-29 03:02:56 -0800

    Finish all implementations and pass all tests

--- SimpleDomainModel/SimpleDomainModel/main.swift
+++ SimpleDomainModel/SimpleDomainModel/main.swift
@@ -101,7 +101,7 @@ open class Job {
     open func calculateIncome(_ hours: Int) -> Int {
         switch self.type {
         case .Hourly(let hourlyWage):
-            return Int(hourlyWage) * hours
+            return Int((hourlyWage) * Double(hours))
         case .Salary(let yearlySalary):
             return yearlySalary
         }
@@ -117,54 +117,88 @@ open class Job {
     }
 }
 
-//////////////////////////////////////
-//// Person
-////
-//open class Person {
-//  open var firstName : String = ""
-//  open var lastName : String = ""
-//  open var age : Int = 0
+////////////////////////////////////
+// Person
 //
-//  fileprivate var _job : Job? = nil
-//  open var job : Job? {
-//    get { }
-//    set(value) {
-//    }
-//  }
-//
-//  fileprivate var _spouse : Person? = nil
-//  open var spouse : Person? {
-//    get { }
-//    set(value) {
-//    }
-//  }
-//
-//  public init(firstName : String, lastName: String, age : Int) {
-//    self.firstName = firstName
-//    self.lastName = lastName
-//    self.age = age
-//  }
-//
-//  open func toString() -> String {
-//  }
-//}
-//
-//////////////////////////////////////
-//// Family
-////
-//open class Family {
-//  fileprivate var members : [Person] = []
-//
-//  public init(spouse1: Person, spouse2: Person) {
-//  }
-//
-//  open func haveChild(_ child: Person) -> Bool {
-//  }
-//
-//  open func householdIncome() -> Int {
-//  }
-//}
+open class Person {
+    open var firstName : String = ""
+    open var lastName : String = ""
+    open var age : Int = 0
 
+    fileprivate var _job : Job? = nil
+    open var job : Job? {
+        get { return self._job}
+        set(value) {
+            self._job = self.age > 15 ? value : nil
+        }
+    }
+
+    fileprivate var _spouse : Person? = nil
+    open var spouse : Person? {
+        get { return self._spouse}
+        set(value) {
+            self._spouse = self.age >= 18 ? value: nil
+        }
+    }
+
+    public init(firstName : String, lastName: String, age : Int) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.age = age
+    }
+
+    open func toString() -> String {
+    var job = ""
+    switch self._job?.type {
+    case .Salary(let salary)?:
+        job = "Salary\(String(describing: salary))"
+    case .Hourly(let hourly)?:
+        job = "Hourly\(hourly)"
+    default:
+        job = "nil"
+    }
+
+    return "[Person: firstName:\(self.firstName) lastName:\(self.lastName) age:\(self.age) job:\(job) spouse:\(String(describing: self._spouse?.firstName ?? nil))]"
+    }
+}
+
+////////////////////////////////////
+//Family
+
+open class Family {
+    fileprivate var members : [Person] = []
+
+    public init(spouse1: Person, spouse2: Person) {
+        if spouse1.spouse == nil && spouse2.spouse == nil {
+            spouse1.spouse = spouse2
+            spouse2.spouse = spouse1
+            self.members.append(spouse1)
+            self.members.append(spouse2)
+        }
+    }
+
+    open func haveChild(_ child: Person) -> Bool {
+        var haveChild = false
+        self.members.forEach { (mem) in
+            if(mem.age >= 21 && mem.spouse != nil){
+                haveChild = true
+            }
+        }
+        if haveChild {
+            self.members.append(child)
+        }
+        return haveChild
+    }
+
+  open func householdIncome() -> Int {
+    return self.members.reduce(0, { (result:Int, mem:Person) -> Int in
+        if let income = mem.job?.calculateIncome(2000) {
+            return result + income
+        }
+        return result
+    })
+  }
+}
 
 
 
